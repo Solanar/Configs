8839a249f00d1b321ec5143f399e05f4ac1e38a2
Solanar <and_555@hotmail.com>
2016-11-02 13:44:00 -0600

    add support for setting up a Canadian dictionary in sublime, add formatting/messages to output

--- setup_env.sh
+++ setup_env.sh
@@ -4,7 +4,8 @@
 echo -e "setting up environment...\n"
 
 # path to this repo
-configs="antyc_solutions\projects\configs-dev\configs"
+projects="antyc_solutions\projects"
+configs="$projects\configs-dev\configs"
 
 # nano
 is_nano_installed=true
@@ -19,6 +20,11 @@ sublime_files=(
 	"GitGutter.sublime-settings"
 	"PythonBreakpoints.sublime-settings"
 	"Anaconda.sublime-settings"
+)
+sublime_dictionary_files=(
+	"English (Canadian).aff"
+	"English (Canadian).dic"
+	"English (Canadian).txt"
 )
 is_sublime_installed=true
 type sublime >/dev/null 2>&1 || { # this arcane code removes output
@@ -47,8 +53,11 @@ if [[ "$OSTYPE" == "msys" ]]; then # Windows
 		cmd //c mklink "$link" "$target"
 	}
 
+	echo -e "linking...\n\n"
+
 	# bash
 	# .bashrc
+	echo "bash dot files"
 	file=".bashrc"
 	link="$HOME/$file"
 	target="$1\\$configs\Windows\\$file"
@@ -63,29 +72,50 @@ if [[ "$OSTYPE" == "msys" ]]; then # Windows
 	link="$HOME/$file"
 	target="$1\\$configs\Windows\\$file"
 	windows_symlink "$file" "$link" "$target"
-	echo ""
+	echo -e "\n"
 
 	# nano
 	if [[ "$is_nano_installed" = true ]]; then
 		# .nanorc
+		echo "nano dot file"
 		file=".nanorc"
 		link="$HOME/$file"
 		target="$1\\$configs\\$file"
 		windows_symlink "$file" "$link" "$target"
-		echo ""
+		echo $"\n"
 	fi
 
 	# sublime
 	if [[ "$is_sublime_installed" = true ]]; then
-		sublime_path="$HOME/AppData/Roaming/Sublime Text 3/Packages/User"
+		echo "sublime settings files"
+		sublime_path="$HOME/AppData/Roaming/Sublime Text 3/Packages"
 
+		sublime_user_path="$sublime_path/User"
 		for file in "${sublime_files[@]}"
 		do
-			link="$sublime_path/$file"
+			link="$sublime_user_path/$file"
 			target="$1\\$configs\Sublime\User\\$file"
 			windows_symlink "$file" "$link" "$target"
 		done
-		# echo "" # uncomment if adding more symlinks
+		echo ""
+
+		# sublime dictionary
+		sublime_dictionary_path="$sublime_path/Dictionaries"
+		if [[ ! -d "$sublime_dictionary_path" ]]; then
+			mkdir "$sublime_dictionary_path"
+		fi
+		if [[ ! -d "$1\\$projects\Dictionaries" ]]; then
+			echo "Could not find Dictionaries repo"
+		else
+			echo "sublime dictionary files"
+			for file in "${sublime_dictionary_files[@]}"
+			do
+				link="$sublime_dictionary_path/$file"
+				target="$1\\$projects\Dictionaries\\$file"
+				windows_symlink "$file" "$link" "$target"
+			done
+		fi
+		echo -e "\n"
 	fi
 # elif [[ "$OSTYPE" == "" ]]; then # Linux
 #	# .bashrc
@@ -93,3 +123,5 @@ if [[ "$OSTYPE" == "msys" ]]; then # Windows
 else
 	echo "Unsupported OSTYPE: $OSTYPE"
 fi
+
+echo "done"
